cb07fd17fdf9c5195e4d48da068a07a0f7820b82
lirinna <37187549+lirinna@users.noreply.github.com>
2018-04-02 16:59:07 +0200

    Add MasterListFragment

---
+++ app/src/main/java/com/example/android/android_me/ui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.android_me.R;
+
+/**
+ * Created by Katy on 02.04.2018.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android/android_me/ui/MasterListFragment.java
@@ -0,0 +1,40 @@
+package com.example.android.android_me.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+/**
+ * Created by Katy on 02.04.2018.
+ */
+
+// This fragment displays all of the AndroidMe images in one large list
+// The list appears as a grid of images
+public class MasterListFragment extends Fragment {
+
+    public MasterListFragment() {
+    }
+
+    // Inflates the GridView of all AndroidMe images
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.images_grid_view);
+
+        // Create the adapter
+        // This adapter takes in the context and an ArrayList of ALL the image resources to display
+        MasterListAdapter mAdapter = new MasterListAdapter(getContext(), AndroidImageAssets.getAll());
+
+        gridView.setAdapter(mAdapter);
+        return rootView;
+    }
+}
